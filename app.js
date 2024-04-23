102799d5814bdfee4ba6a80c0d02a4b3c062c218
admmintimser <adminti@timsergroup.com>
2024-04-23 10:54:16 -0600

    Folder refactor
    Refactor

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ import appointmentRouter from "./router/appointmentRouter.js";
 
 const app = express();
 
-app.use(express.static("../frontend/dist"));
+app.use(express.static("./frontend/dist"));
 app.get("*",(req,res) =>{
   res.sendFile(path.resolve(__dirname,"frontend", "dist", "index.html"))
 });
