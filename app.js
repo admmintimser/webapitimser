5ff80882392b18bce8b790804c936d75d715f407
admmintimser <adminti@timsergroup.com>
2024-04-26 15:42:34 -0600

    Update app.js

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(errorMiddleware);
 // Connect to Database
 connectToDB();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
