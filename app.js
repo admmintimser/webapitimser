345cef1cf3827e6cbffdfe33c68c4a5ff047c916
admmintimser <adminti@timsergroup.com>
2024-04-29 10:34:55 -0600

    Update app.js

--- app.js
+++ app.js
@@ -9,10 +9,7 @@ import { errorMiddleware } from "./middlewares/error.js";
 import messageRouter from "./router/messageRouter.js";
 import userRouter from "./router/userRouter.js";
 import appointmentRouter from "./router/appointmentRouter.js";
-import path from 'path';
-import { fileURLToPath } from 'url';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
@@ -25,22 +22,6 @@ app.use(helmet());
 app.use("/api/v1/message", messageRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/appointment", appointmentRouter);
-
-// Serve Frontend SPA
-const frontendPath = path.join(__dirname, '../frontend/dist');
-app.use(express.static(frontendPath));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(frontendPath, 'index.html'));
-});
-
-// Serve Dashboard SPA
-const dashboardPath = path.join(__dirname, '../dashboard/dist');
-app.use('/dashboard', express.static(dashboardPath));
-app.get('/dashboard/*', (req, res) => {
-  res.sendFile(path.join(dashboardPath, 'index.html'));
-});
-
-
 
 // Error Middleware
 app.use(errorMiddleware);
