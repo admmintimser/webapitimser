c89ae326972614f26cc5e15ded05015e4beade11
admmintimser <adminti@timsergroup.com>
2024-05-22 05:56:50 -0600

    Update appointmentController.js

--- controller/appointmentController.js
+++ controller/appointmentController.js
@@ -3,13 +3,44 @@ import ErrorHandler from "../middlewares/error.js";
 import { Appointment } from "../models/appointmentSchema.js";
 import { User } from "../models/userSchema.js";
 
+
+export const getAllAppointments = catchAsyncErrors(async (req, res, next) => {
+    const appointments = await Appointment.find();
+    res.status(200).json({ success: true, appointments });
+});
+
+export const updateAppointmentStatus = catchAsyncErrors(async (req, res, next) => {
+    const { id } = req.params;
+    let appointment = await Appointment.findById(id);
+    if (!appointment) {
+        return next(new ErrorHandler("Appointment not found!", 404));
+    }
+    appointment = await Appointment.findByIdAndUpdate(id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+    res.status(200).json({ success: true, message: "Appointment Status Updated!" });
+});
+
+export const deleteAppointment = catchAsyncErrors(async (req, res, next) => {
+    const { id } = req.params;
+    const appointment = await Appointment.findById(id);
+    if (!appointment) {
+        return next(new ErrorHandler("Appointment Not Found!", 404));
+    }
+    await appointment.deleteOne();
+    res.status(200).json({ success: true, message: "Appointment Deleted!" });
+});
+
+export const countAppointmentsProcessed = catchAsyncErrors(async (req, res, next) => {
+    const processedCount = await Appointment.countDocuments({ tomaProcesada: true });
+    res.status(200).json({ success: true, count: processedCount });
+});
+
 export const countAppointmentsNotProcessed = catchAsyncErrors(async (req, res, next) => {
     const notProcessedCount = await Appointment.countDocuments({ tomaProcesada: false });
     res.status(200).json({ success: true, count: notProcessedCount });
-});
-export const countAppointmentsProcessed = catchAsyncErrors(async (req, res, next) => {
-    const ProcessedCount = await Appointment.countDocuments({ tomaProcesada: true });
-    res.status(200).json({ success: true, count: ProcessedCount });
 });
 export const postAppointment = catchAsyncErrors(async (req, res, next) => {
     const {
