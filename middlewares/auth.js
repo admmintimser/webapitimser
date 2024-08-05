08ed3729c73706058ebacc6ab74d183a0c30b4a6
admmintimser <adminti@timsergroup.com>
2024-08-05 10:14:24 -0600

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,5 @@
 import { User } from "../models/userSchema.js";
-import { catchAsyncErrors } from "./catchAsyncErrors.js";
+import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
 import ErrorHandler from "./error.js";
 import jwt from "jsonwebtoken";
 
@@ -33,6 +33,24 @@ export const isPatientAuthenticated = catchAsyncErrors(
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     req.user = await User.findById(decoded.id);
     if (req.user.role !== "Patient") {
+      return next(
+        new ErrorHandler(`${req.user.role} not authorized for this resource!`, 403)
+      );
+    }
+    next();
+  }
+);
+
+// Middleware to authenticate reception users
+export const isReceptionistOrAdminAuthenticated = catchAsyncErrors(
+  async (req, res, next) => {
+    const token = req.cookies.receptionToken || req.cookies.adminToken;
+    if (!token) {
+      return next(new ErrorHandler("Receptionist or Admin is not authenticated!", 400));
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = await User.findById(decoded.id);
+    if (req.user.role !== "Receptionist" && req.user.role !== "Admin") {
       return next(
         new ErrorHandler(`${req.user.role} not authorized for this resource!`, 403)
       );
